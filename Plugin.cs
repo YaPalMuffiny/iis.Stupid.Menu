88ec4d4581f7ca89b54715aec125b96b056b8cb3
Nolan <canadacf18Bloxs@gmail.com>
2025-04-04 21:11:32 -0700

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -5,19 +5,39 @@ using UnityEngine;
 namespace iiMenu
 {
     [System.ComponentModel.Description(PluginInfo.Description)]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")] // Make sure to add Utilla 1.5.0 as a dependency!
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    [ModdedGamemode] // Enable callbacks in default modded gamemodes
     public class Plugin : BaseUnityPlugin
     {
-        private void Start() // To that one dude that uses SMI to inject my menu, it's this method
-        {
-            Console.Title = "ii's Stupid Menu // Build " + PluginInfo.Version;
+        bool inAllowedRoom = false;
 
-            iiMenu.Patches.Menu.ApplyHarmonyPatches();
-            GameObject Loading = new GameObject("ii");
-            Loading.AddComponent<iiMenu.UI.Main>();
-            Loading.AddComponent<iiMenu.Notifications.NotifiLib>();
-            Loading.AddComponent<iiMenu.Classes.CoroutineManager>();
-            UnityEngine.Object.DontDestroyOnLoad(Loading);
+        private void Update()
+        {
+            if (inAllowedRoom)
+            {
+                Console.Title = "ii's Stupid Menu // Build " + PluginInfo.Version;
+                iiMenu.Patches.Menu.ApplyHarmonyPatches();
+                GameObject Loading = new GameObject("ii");
+                Loading.AddComponent<iiMenu.UI.Main>();
+                Loading.AddComponent<iiMenu.Notifications.NotifiLib>();
+                Loading.AddComponent<iiMenu.Classes.CoroutineManager>();
+                UnityEngine.Object.DontDestroyOnLoad(Loading);
+            }
+        }
+
+        [ModdedGamemodeJoin]
+        private void RoomJoined(string gamemode)
+        {
+            // The room is modded. Enable mod stuff.
+            inAllowedRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        private void RoomLeft(string gamemode)
+        {
+            // The room was left. Disable mod stuff.
+            inAllowedRoom = false;
         }
     }
 }
